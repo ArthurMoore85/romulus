cdfc93c5c7c1fe661e0f070272550502e7b701d0
Arthur Moore <arthur.moore85@gmail.com>
2016-11-26 10:31:32 +0000

    Added filter combobox in Pi Control Centre

--- ui/pi_controller.py
+++ ui/pi_controller.py
@@ -17,27 +17,64 @@ class PiWindow(QtGui.QMainWindow, Ui_PiWindow):
             self.lblPiStatus.setText('<h2>{0}</h2>'.format(self.sync_obj.status_message))
         self.games = self.sync_obj.remote_library
         self.comboFilter.addItem('All')
+        self.pi_header = None
+        self.pi_headers = None
         self.comboFilter.addItems(self.games.keys())
         self._default_settings()
+
+        # Triggers
+        self.comboFilter.activated.connect(self._filter_results)
 
     def _default_settings(self):
         """
         Sets default settings
         """
-        pi_headers = ['Title', 'Platform']
+        self.pi_headers = ['Title', 'Platform']
         self.tableLibrary.setColumnCount(2)
-        self.tableLibrary.setHorizontalHeaderLabels(pi_headers)
-        pi_header = self.tableLibrary.horizontalHeader()
+        self.tableLibrary.setHorizontalHeaderLabels(self.pi_headers)
+        self.pi_header = self.tableLibrary.horizontalHeader()
+        self._default_table()
+
+    def _default_table(self):
+        """
+        Sets default table data
+        """
+        self.tableLibrary.clear()
         results = 0
         for k, v in self.games.iteritems():
             for x in v:
                 results += 1
         self.tableLibrary.setRowCount(results)
-        pi_header.setStretchLastSection(False)
-        pi_header.setResizeMode(0, QHeaderView.Stretch)
+        self.pi_header.setStretchLastSection(False)
+        self.pi_header.setResizeMode(0, QHeaderView.Stretch)
+        self.tableLibrary.setHorizontalHeaderLabels(self.pi_headers)
         row = 0
         for platform, games in self.games.iteritems():
             for game in games:
                 self.tableLibrary.setItem(row, 0, QtGui.QTableWidgetItem(game))
                 self.tableLibrary.setItem(row, 1, QtGui.QTableWidgetItem(platform))
                 row += 1
+        self.comboFilter.addItem('All')
+        self.comboFilter.addItems(self.games.keys())
+
+    def _filter_results(self):
+        """
+        Shows only results based on combobox selection
+        """
+        combo_selection = str(self.comboFilter.currentText())
+        if combo_selection == 'All':
+            self._default_table()
+        else:
+            self.pi_header.setStretchLastSection(False)
+            self.pi_header.setResizeMode(0, QHeaderView.Stretch)
+            self.tableLibrary.setHorizontalHeaderLabels(self.pi_headers)
+            results = self.games[combo_selection]
+            self.comboFilter.clear()
+            self.tableLibrary.setRowCount(len(results))
+            row = 0
+            for game in results:
+                self.tableLibrary.setItem(row, 0, QtGui.QTableWidgetItem(game))
+                self.tableLibrary.setItem(row, 1, QtGui.QTableWidgetItem(combo_selection))
+                row += 1
+        self.comboFilter.addItem('All')
+        self.comboFilter.addItems(self.games.keys())
